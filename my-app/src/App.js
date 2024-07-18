fe38789bf3b7204f86f97cb16f00d8b93388c82a
sneha404 <snehajaisinghani3005@gmail.com>
2024-07-18 08:04:20 +0530

    Initial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,211 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <>
+
+
+      <div className="container-fluid">
+        <div className=" row r3">
+          <div className="img">
+            <div className="col-lg-12 txt1">
+              We Develop Custom
+            </div>
+            <div className="col-lg-12 txt2">
+              Investing Strategy
+            </div>
+            <div className="col-lg-12 txt3">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente quis assumenda quas architecto! Ipsam eveniet
+            </div>
+            <div className="col-lg-12 txt4">
+              <button>
+                Start Today
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <div className="row r4">
+          <div className="col-lg-12 c1">
+            <div className="box1">
+              <div className="txt1">Market</div>
+              <div className="txt2">Forecasting</div>
+              <div className="txt3">Marketing Forecasting is a core component of a market analysis</div>
+              <div className="line"></div>
+              <div className="txt4">View More <span><i class="fa fa-arrow-right" aria-hidden="true"></i></span></div>
+            </div>
+            <div className="box2">
+              <div className="txt1">Market</div>
+              <div className="txt2">Forecasting</div>
+              <div className="txt3">Marketing Forecasting is a core component of a market analysis</div>
+              <div className="line"></div>
+              <div className="txt4">View More <span><i class="fa fa-arrow-right" aria-hidden="true"></i></span></div>
+            </div>
+            <div className="box3">
+              <div className="txt1">Market</div>
+              <div className="txt2">Forecasting</div>
+              <div className="txt3">Marketing Forecasting is a core component of a market analysis</div>
+              <div className="line"></div>
+              <div className="txt4">View More <span><i class="fa fa-arrow-right" aria-hidden="true"></i></span></div>
+            </div>
+          </div>
+
+
+
+          <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat ab sint nulla at ut dolorem eveniet tempora praesentium, nisi esse modi ipsum exercitationem, obcaecati et voluptates error adipisci delectus. Nulla.</h1>
+
+        </div>
+
+        <div className="row r6">
+          <div className="img">
+            <div className="col-lg-12 txt1">
+              Getting Help is Easy
+            </div>
+            <div className="col-lg-12 txt2">
+              Have a question? Give us a call or stop by for a quote. It's that easy
+            </div>
+            <div className="col-lg-12">
+              <button>Contact us</button>
+            </div>
+          </div>
+        </div>
+
+        <div className="row r7">
+          <div className="col-lg-12 text1">Our Services</div>
+          <div className="col-lg-12 text2">Our company provides various services to our customers. Check out our services for more available</div>
+          <div className="col-lg-4 cc1">
+            <div className="img"></div>
+            <div className="txt1">Strategy Development</div>
+            <div className="txt2">Quisque malesuada sed nibh nec facilisis isa. Aliquam at tincidunt odio</div>
+          </div>
+        </div>
+
+        <div className="row r8">
+          <div className="img">
+            <div className="col-lg-12">
+              <div className="button"><button><i class="fa fa-play" aria-hidden="true"></i></button></div>
+            </div>
+            <div className="col-lg-12 text1">
+              Video Presentations
+            </div>
+            <div className="col-lg-12 text2">
+              some examples of companies that responses to these criteria
+            </div>
+          </div>
+        </div>
+
+        <div className="row r9">
+          <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus recusandae rem assumenda tempore, itaque voluptates esse iste vel autem ex nesciunt laudantium odit. Inventore minima culpa, accusantium et mollitia eligendi?</h1>
+        </div>
+
+        <div className="row r10">
+          <div className="img">
+            <div className="col-lg-12 text1">
+              Our Pricing Plans
+            </div>
+            <div className="col-lg-12 text2">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum veritatis voluptates numquam asperiores cum quisquam explicabo
+            </div>
+          </div>
+        </div>
+
+        <div className="row r11">
+          <div className="col-lg-12 cc1">
+            <div className="box1 boxcc">
+              <div className="icon"></div>
+              <div className="txt1">Starter Plan</div>
+              <div className="txt2">$ 110</div>
+              <div className="txt3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure obcaecati repellat perspiciatis minima, voluptates.
+              </div>
+              <div className="button"><button>Get Started</button></div>
+            </div>
+            <div className="box2 boxcc">
+              <div className="icon"></div>
+              <div className="txt1">Basic Plan</div>
+              <div className="txt2">$ 145</div>
+              <div className="txt3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure obcaecati repellat perspiciatis minima, voluptates.
+              </div>
+              <div className="button"><button>Get Started</button></div>
+            </div>
+            <div className="box3 boxcc">
+              <div className="icon"></div>
+              <div className="txt1">Advanced Plan</div>
+              <div className="txt2">$ 175</div>
+              <div className="txt3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure obcaecati repellat perspiciatis minima, voluptates.
+              </div>
+              <div className="button"><button>Get Started</button></div>
+            </div>
+          </div>
+        </div>
+
+        <div className="row r12">
+          <div className="col-lg-12 heading">What People Say</div>
+          <div className="col-lg-6 cc1">
+            <div className="img"></div>
+            <div className="txt1">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis labore omnis obcaecati inventore odit doloribus? Consectetur corporis dolor voluptatibus praesentium deleniti.
+            </div>
+            <div className="txt2">Martine Cloud</div>
+            <div className="txt3">Investor</div>
+          </div>
+          <div className="col-lg-6 cc2">
+            <div className="img"></div>
+            <div className="txt1">
+              Perferendis illo recusandae voluptas veritatis libero maiores praesentium, at voluptatum reiciendis, blanditiis nobis. Obcaecati nobis cupiditate quas voluptas minima, sint quisquam reiciendis?
+            </div>
+            <div className="txt2">Sem Rubick</div>
+            <div className="txt3">Investor</div>
+          </div>
+          <div className="col-lg-12">
+            <i class="fa fa-circle-o" aria-hidden="true"></i>
+            <i class="fa fa-circle" aria-hidden="true"></i>
+          </div>
+        </div>
+
+        <div className="row r14">
+          <div className="col-lg-6">
+            <div className="heading1">Our Contacts</div>
+            <div className="text1">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quibusdam id exercitationem ullam cum natus tempore atque voluptatem perferendis corporis facilis</div>
+            <div className="heading2">Address:</div>
+            <div className="text2">123 King street. Melbourne , Australia</div>
+            <div className="heading3">Phones:</div>
+            <div className="text3">800-567-1234</div>
+            <div className="text4">800-327-1235</div>
+            <div className="heading4">Email:</div>
+            <div className="text5">info@example.com</div>
+            <i class="fa fa-facebook" aria-hidden="true"></i>
+            <i class="fa fa-google-plus" aria-hidden="true"></i>
+            <i class="fa fa-linkedin" aria-hidden="true"></i>
+            <i class="fa fa-twitter" aria-hidden="true"></i>
+          </div>
+          <div className="col-lg-6">
+            <div className="head1">Ask Us a Question</div>
+            <div className="txt1">voluptatibus reprehenderit dignissimos, animi dolore dicta doloribus provident, ad ut eius assumenda. Labore laudantium consectetur nam corrupti doloribus!</div>
+            <div className="input1"><input className='form-control mb-2 mt-5' type="text" placeholder='Your Name' /></div>
+            <div className="input2">
+              <textarea className='form-control' rows={5} placeholder='message' name="" id=""></textarea>
+            </div>
+            <div className="button"><button>Send Message</button></div>
+          </div>
+        </div>
+        <hr />
+        <div className="row r15">
+          <div className="col-lg-6">
+            <span>Terms and Conditions |</span>
+            <span> Privacy Policy | </span>
+            <span><i class="fa fa-circle-o" aria-hidden="true"></i></span>
+            <span>2020 Investments.All rights reserved</span>
+          </div>
+          <div className="col-lg-6 cc1">
+            <span>Home</span>
+            <span>About</span>
+            <span>Services</span>
+            <span>Team</span>
+            <span>Pricing</span>
+            <span>Contacts</span>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
